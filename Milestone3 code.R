b09ba6aa4925041cbc105a1ec947cc5882612119
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-10-10 13:57:26 -0400

    Update Milestone3 code.R
    R Code updated to reflect the x and y axis changes on the graphs.

--- Milestone3 code.R
+++ Milestone3 code.R
@@ -1,4 +1,4 @@
-hist(Squirrel$`Number of sighters`, main = "Squirrel Sightings", xlab = "Number of Sightings", ylab = "How Often")
-barplot(table(Squirrel$`Number of Squirrels`), main = "Number Of Squirrels", xlab = "Number of Squirrels", ylab = "How often")
-hist(Squirrel$`Total Time of Sighting`, main = "Total Times of Sighting", xlab = "Number of sighting", ylab = "How often")
+hist(Squirrel$`Number of sighters`, main = "Squirrel Sightings", xlab = "Number of Sighters", ylab = "How Times the Squirrel was seen")
+barplot(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Number of sighters`), main = "Sighters", xlab = "Number of Sighters", ylab = "Sighting Sessions")
+hist(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Total Time of Sighting`), main = "Total Times of Sighting", xlab = "Session Time", ylab = "Number of Sessions")
 barplot(table(Squirrel$Litter), main = "Litter", ylab = "Litter Size", xlab = "Amount")
