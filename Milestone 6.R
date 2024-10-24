c832b120fee144214f6dbf831eaa5820ab47721e
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-10-24 13:48:31 -0400

    Milestone 6

---
+++ Milestone 6.R
@@ -0,0 +1,2 @@
+plot(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Total Time of Sighting`, X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Number of Squirrels`, main = "Scatterplot", xlab = "Total Time of Sighting", ylab = "Number of Squirrels")
+cor(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Total Time of Sighting`, X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Number of Squirrels`)
