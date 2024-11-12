5eacbf3915b036fe9b02213ff3bd6e825581195e
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-11-12 14:06:05 -0500

    Update Milestone 6.R
    Updated the correlation code

--- Milestone 6.R
+++ Milestone 6.R
@@ -1,2 +1,6 @@
 plot(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Total Time of Sighting`, X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Number of Squirrels`, main = "Scatterplot", xlab = "Total Time of Sighting", ylab = "Number of Squirrels")
 cor(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Total Time of Sighting`, X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Number of Squirrels`)
+
+
+//Had to add a piece of code so that it will block out the N/A in the data
+cor(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Total Time of Sighting`, X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$`Number of Squirrels`, use = "complete.obs")
