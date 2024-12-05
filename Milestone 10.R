728ddc94992950d57e77befc12aae79b164edf4f
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-12-05 13:56:48 -0500

    Milestone 10

---
+++ Milestone 10.R
@@ -0,0 +1,2 @@
+barplot(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$Date))
+barplot(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$Date), main = "Date of Squirrel Sighting", xlab = "Date", ylab = "Amount of Squirrels seen in one day")
