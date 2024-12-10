6ac0d24de9db9c7b22ff6edbd343ef34557e3f7c
Jdeaza78 <159087749+Jdeaza78@users.noreply.github.com>
2024-12-10 10:27:24 -0500

    Update Milestone 10.R
    Added the las = 2  it turns the date labels perpendicular to the axis.

--- Milestone 10.R
+++ Milestone 10.R
@@ -1,2 +1,3 @@
 barplot(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$Date))
 barplot(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$Date), main = "Date of Squirrel Sighting", xlab = "Date", ylab = "Amount of Squirrels seen in one day")
+barplot(table(X2018_Central_Park_Squirrel_Census_Hectare_Data_20240919$Date), las = 2, main = "Date of Squirrel Sighting", xlab = "Date", ylab = "Amount of Squirrels seen in one day")
